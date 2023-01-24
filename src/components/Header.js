99e51da63e676a90ee220b1bf5be58969d0cde57
Laura <LNKEAT@gmail.com>
2023-01-24 11:10:09 -0700

    update index style sheet with all styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,10 @@ import React from 'react'
 const Header = ({name}) => {
   return (
     <header>
-        <h1>Task Tracker</h1>
+        <h1 style={{color: 'red', backgroundColor: 'black'}}> Task Tracker</h1>
         <h2>My name is {name}</h2>
     </header>
   ) 
 } 
-
+ 
 export default Header
