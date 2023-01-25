cca14409bc5e11912bf92a0f651be427eb3ae1b9
Laura <LNKEAT@gmail.com>
2023-01-25 14:13:15 -0700

    create task list array and state

--- src/App.js
+++ src/App.js
@@ -5,30 +5,31 @@ import Tasks from './components/Tasks';
 
 function App() {
   const title = 'Task Tracker'
+
   const [taskList, setTaskList] = useState(
     [
-        {
-            id: 1, 
-            text: 'Doctor Appointment',
-            day: 'Feb 5th, 2:30pm',
-            reminder: true
-        },
-        {
-            id: 2, 
-            text: 'Clean room',
-            day: 'Feb 12th, 9:00pm',
-            reminder: true
-        },
-        {
-            id:3, 
-            text: 'Sing in the Shower',
-            day: 'Jan 1st, 12:00am',
-            reminder: false
-        }
-    ]
-)
+      {
+          "id": 1, 
+          "text": "Doctor Appointment",
+          "day": "Feb 5th, 2:30pm",
+          "reminder": true
+      }, 
+      {
+          "id": 2, 
+          "text": "Clean room",
+          "day": "Feb 12th, 9:00pm",
+          "reminder": true
+      },
+      {
+          "id":3, 
+          "text": "Sing in the Shower",
+          "day": "Jan 1st, 12:00am",
+          "reminder": false
+      }
+  ]
+  )
   
-
+ 
   return (
     <div className="container">
       <Header title={title} />
